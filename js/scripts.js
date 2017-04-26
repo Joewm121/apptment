1a09f442b0e4df9276e0dcbff5a40b7b2181dbce
Connor Larsen and Joe Werderber <connor@>
2017-04-26 15:25:23 -0700

    added js for form function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-// Business logic
-
-var name = $("input#name").val();
-
 // UI logic
+$(function() {
+  $("#appointment").submit(function(event) {
+    var name = $("input#name").val();
+    var description = $("input#description").val()
+    var date = $("input#date").val()
+    var time = $("input#time").val()
+    $(".nameConfirm").text(name);
+    $(".descriptionConfirm").text(description);
+    $(".dateConfirm").text(date);
+    $(".timeConfirm").text(time);
+    $(".container1").fadeToggle();
+    $(".container2").fadeToggle();
+    event.preventDefault();
+  });
+});
